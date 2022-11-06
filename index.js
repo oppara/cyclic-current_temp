ed7da533a6eebb24ebc641cc1a03430546b2e065
oppara <oppara@oppara.tv>
2022-11-06 19:41:31 +0900

    feat: tweets current temp at Tokyo hourly.

---
+++ index.js
@@ -0,0 +1,66 @@
+// import * as dotenv from 'dotenv'
+import dotenv from 'dotenv';
+dotenv.config();
+
+import http from 'http';
+import { formatInTimeZone } from 'date-fns-tz/esm';
+import got from 'got';
+import { TwitterApi } from 'twitter-api-v2';
+
+
+const TZ = 'Asia/Tokyo';
+const AMEDAS_URL_BASE = 'https://www.jma.go.jp/bosai/amedas/data/map/';
+const AMEDAS_ID_TOKYO = '44132';
+
+const getTemperature = async () => {
+  try {
+    const ymdh = formatInTimeZone(new Date(), TZ, 'yyyyMMddHH0000');
+    const url = `${AMEDAS_URL_BASE}${ymdh}.json`;
+    console.log(`url: ${url}`);
+
+    const data = await got.get(url).json();
+
+    if (AMEDAS_ID_TOKYO in data) {
+      return data[AMEDAS_ID_TOKYO]['temp'][0];
+    }
+
+    throw new Error('could not retreive tokyo data.');
+
+  } catch (error) {
+    error.message = error.response.body;
+    throw error;
+  }
+};
+
+const makeMessage = (temp) => {
+  const dt = formatInTimeZone(new Date(), TZ, 'kaaa MMM do');
+  return `${temp}℃ ${dt}`;
+};
+
+const tweet = async (message) => {
+  const client = new TwitterApi({
+    appKey: process.env.APP_KEY,
+    appSecret: process.env.APP_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET,
+  });
+
+  await client.v1.tweet(message);
+};
+
+http.createServer(async (req, res) => {
+  try {
+    console.log(`reg.url: ${req.url}`);
+
+    const temp = await getTemperature();
+    const message = await makeMessage(temp);
+    console.log(`message: ${message}`);
+
+    await tweet(message);
+
+    res.write('done.');
+    res.end();
+  } catch (error) {
+    console.log(`ERROR: ${error.message}`);
+  }
+}).listen(process.env.PORT || 3000);
